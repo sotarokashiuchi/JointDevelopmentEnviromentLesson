eecc2806a82c78c7b6776bc8874ab9c6a00d6b7f
Kashiuchi Sotaro <sotaro0416@gmail.com>
2023-08-20 11:31:27 +0900

    main.c selecet mene

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+	int menu_id;
+	for( ; ; ){
+		/* メニュー選択 */
+		// メニュー表示
+		printf("メニュー 一覧\n");
+		printf(" 0占い終了\n");
+		printf(" 1今日の運勢を占う\n");
+		printf(" 2星座占い\n");
+		printf("メニューを数値で選択してください>>>\n");
+
+		/* 占い呼び出し*/
+
+	}
+
+	return 0;
+}
