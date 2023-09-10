d4ce9f87a6767be659ccf007a8606d9075d87418
Kashiuchi Sotaro <sotaro0416@gmail.com>
2023-09-10 13:29:33 +0900

    getSoulNumber

--- Go/main_sample.go
+++ Go/main_sample.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(getSoulNumber(20060416))
+}
+
+func getSoulNumber(number int) (soulNumber int) {
+	for i := 0; i < 8; i++ {
+		soulNumber += number % 10
+		number = number / 10
+		if soulNumber >= 10 {
+			soulNumber = soulNumber%10 + (soulNumber%100)/10
+		}
+
+	}
+	return soulNumber
 }
