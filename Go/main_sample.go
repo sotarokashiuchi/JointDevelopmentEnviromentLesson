79a99711fb32ae551c9efde459a9734f8b1d18a6
Kashiuchi Sotaro <sotaro0416@gmail.com>
2023-09-10 13:13:16 +0900

    input

--- Go/main_sample.go
+++ Go/main_sample.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	var inputNumber int
+	inputNumber = input()
+	fmt.Println(inputNumber)
+}
+
+func input() int {
+	var inputNumber int
+	fmt.Println("数値を入力してください")
+	fmt.Scan(&inputNumber)
+	return inputNumber
 }
